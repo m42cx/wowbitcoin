147ee25ceface15bf3dc2fa973bedbef1fb83e8a
xziy <xziy@localhost.localdomain>
2018-12-20 07:35:48 +0500

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Wow-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "wowURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
